ebe8e1bafe1021bc621f38d504178a56e2c17b4f
vijayrathore727 <vijayrathore727@gmail.com>
2024-10-05 19:36:33 +0530

    add multiple and divide method

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,12 @@
 function add(a, b) {
     console.log('adding'+a+b)
 }
+
+
+function multiple(a, b){
+    console.log('multiplying'+a*b);
+}
+
+function divide(a, b){
+        console.log('Dividing'+a/b);
+}
