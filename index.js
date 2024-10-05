890d74285979e21877afeee8d5c2e95c04c7fa1f
VIJAY <68022329+vijayrathore727@users.noreply.github.com>
2024-10-05 21:24:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const age=24;
 const name='vijay rathore';
 const city='indore';
-const subLocation='indore';
+const subLocation='Gwalior';
 function sayHello(){
     console.log(`Hello, my name is ${name}. I am ${age} years old. I live in ${city}, ${subLocation}.`);
 }
