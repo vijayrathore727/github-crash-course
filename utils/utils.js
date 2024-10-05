acf7af6323a753a8562bd475d3f9ed80d1b3a442
vijayrathore727 <vijayrathore727@gmail.com>
2024-10-05 20:47:51 +0530

    factorail method add

--- utils/utils.js
+++ utils/utils.js
@@ -10,3 +10,12 @@ function multiple(a, b){
 function divide(a, b){
         console.log('Dividing'+a/b);
 }
+
+function factorial(n){
+    if(n === 0){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+}
